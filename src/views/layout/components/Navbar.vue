c456f967d715bfc403480d21cddd7ae37e97af50
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-15 16:19:03 +0800

    Add: nidemall and author names

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -32,13 +32,8 @@
             </el-dropdown-item>
           </router-link>
           <el-dropdown-item divided>
-            <a target="_blank" href="https://github.com/linlinjava/litemall">
+            <a target="_blank" href="https://github.com/charlyisidore/nidemall-admin">
               GitHub
-            </a>
-          </el-dropdown-item>
-          <el-dropdown-item>
-            <a target="_blank" href="https://gitee.com/linlinjava/litemall">
-              码云
             </a>
           </el-dropdown-item>
           <el-dropdown-item divided>
