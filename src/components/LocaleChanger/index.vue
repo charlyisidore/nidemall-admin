77d039b6ef2915cbc3f930931b8cc6549828a223
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-07-27 13:58:19 +0800

    Add: LocaleChanger component to switch language

---
+++ src/components/LocaleChanger/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-select v-model="$i18n.locale" size="mini" placeholder="lang" class="select">
+    <el-option v-for="(lang, i) in langs" :key="`Lang${i}`" :label="lang" :value="lang" />
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: 'LocaleChanger',
+  data() {
+    return { langs: Object.keys(this.$i18n.messages) }
+  }
+}
+</script>
+
+<style>
+.select {
+  width: 112px;
+}
+</style>
