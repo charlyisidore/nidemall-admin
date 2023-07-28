1feccaed28404c094519926ff0ad458d23445dad
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-07-28 09:41:00 +0800

    Change: reorganize translation keys (sidebar and breadcrumb)

--- src/locales/en.js
+++ src/locales/en.js
@@ -31,165 +31,61 @@ export default {
       logout: 'Logout'
     }
   },
-  sidebar: {
-    dashboard: {
-      title: 'Dashboard'
-    },
-    user: {
-      title: 'Users'
-    },
-    user_user: {
-      title: 'Customers'
-    },
-    user_address: {
-      title: 'Addresses'
-    },
-    user_collect: {
-      title: 'Favorites'
-    },
-    user_footprint: {
-      title: 'Footprints'
-    },
-    user_history: {
-      title: 'Search history'
-    },
-    user_feedback: {
-      title: 'Feedbacks'
-    },
-    mall: {
-      title: 'Mall'
-    },
-    mall_region: {
-      title: 'Regions'
-    },
-    mall_brand: {
-      title: 'Brands'
-    },
-    mall_category: {
-      title: 'Categories'
-    },
-    mall_order: {
-      title: 'Orders'
-    },
-    mall_aftersale: {
-      title: 'After-sales'
-    },
-    mall_issue: {
-      title: 'Issues'
-    },
-    mall_keyword: {
-      title: 'Keywords'
-    },
-    goods: {
-      title: 'Products'
-    },
-    goods_list: {
-      title: 'List products'
-    },
-    goods_create: {
-      title: 'Add product'
-    },
-    goods_edit: {
-      title: 'Edit product'
-    },
-    goods_comment: {
-      title: 'Comments'
-    },
-    promotion: {
-      title: 'Promotions'
-    },
-    promotion_ad: {
-      title: 'Ads'
-    },
-    promotion_coupon: {
-      title: 'Coupons'
-    },
-    promotion_coupon_detail: {
-      title: 'Coupon details'
-    },
-    promotion_topic: {
-      title: 'Topics'
-    },
-    promotion_topic_create: {
-      title: 'Create topic'
-    },
-    promotion_topic_edit: {
-      title: 'Edit topic'
-    },
-    promotion_groupon_rule: {
-      title: 'Groupon rules'
-    },
-    promotion_groupon_activity: {
-      title: 'Groupon activity'
-    },
-    sys: {
-      title: 'System'
-    },
-    sys_admin: {
-      title: 'Admins'
-    },
-    sys_notice: {
-      title: 'Notifications'
-    },
-    sys_log: {
-      title: 'Logs'
-    },
-    sys_role: {
-      title: 'Roles'
-    },
-    sys_os: {
-      title: 'Storage'
-    },
-    config: {
-      title: 'Configuration'
-    },
-    config_mall: {
-      title: 'Mall config'
-    },
-    config_express: {
-      title: 'Freight config'
-    },
-    config_order: {
-      title: 'Order config'
-    },
-    config_wx: {
-      title: 'Mini-program config'
-    },
-    stat: {
-      title: 'Statistics'
-    },
-    stat_user: {
-      title: 'User stats'
-    },
-    stat_order: {
-      title: 'Order stats'
-    },
-    stat_goods: {
-      title: 'Product stats'
-    },
-    external_link: {
-      title: 'External links'
-    },
-    external_link_tencent_cos: {
-      title: 'Tencent cloud storage'
-    },
-    external_link_tencent_sms: {
-      title: 'Tencent cloud SMS'
-    },
-    external_link_weixin_pay: {
-      title: 'WeChat Pay'
-    },
-    external_link_weixin_mpkf: {
-      title: 'WeChat mini-program'
-    },
-    external_link_alibaba_oss: {
-      title: 'Ali cloud storage'
-    },
-    external_link_qiniu_kodo: {
-      title: 'Qiniu cloud storage'
-    },
-    external_link_kdniao_api: {
-      title: 'Kdniao API'
+  app: {
+    menu: {
+      dashboard: 'Dashboard',
+      user: 'Users',
+      user_user: 'Customers',
+      user_address: 'Addresses',
+      user_collect: 'Favorites',
+      user_footprint: 'Footprints',
+      user_history: 'Search history',
+      user_feedback: 'Feedbacks',
+      mall: 'Mall',
+      mall_region: 'Regions',
+      mall_brand: 'Brands',
+      mall_category: 'Categories',
+      mall_order: 'Orders',
+      mall_aftersale: 'After-sales',
+      mall_issue: 'Issues',
+      mall_keyword: 'Keywords',
+      goods: 'Products',
+      goods_list: 'List products',
+      goods_create: 'Add product',
+      goods_edit: 'Edit product',
+      goods_comment: 'Comments',
+      promotion: 'Promotions',
+      promotion_ad: 'Ads',
+      promotion_coupon: 'Coupons',
+      promotion_coupon_detail: 'Coupon details',
+      promotion_topic: 'Topics',
+      promotion_topic_create: 'Create topic',
+      promotion_topic_edit: 'Edit topic',
+      promotion_groupon_rule: 'Groupon rules',
+      promotion_groupon_activity: 'Groupon activity',
+      sys: 'System',
+      sys_admin: 'Admins',
+      sys_notice: 'Notifications',
+      sys_log: 'Logs',
+      sys_role: 'Roles',
+      sys_os: 'Storage',
+      config: 'Configuration',
+      config_mall: 'Mall config',
+      config_express: 'Freight config',
+      config_order: 'Order config',
+      config_wx: 'Mini-program config',
+      stat: 'Statistics',
+      stat_user: 'User stats',
+      stat_order: 'Order stats',
+      stat_goods: 'Product stats',
+      external_link: 'External links',
+      external_link_tencent_cos: 'Tencent cloud storage',
+      external_link_tencent_sms: 'Tencent cloud SMS',
+      external_link_weixin_pay: 'WeChat Pay',
+      external_link_weixin_mpkf: 'WeChat mini-program',
+      external_link_alibaba_oss: 'Ali cloud storage',
+      external_link_qiniu_kodo: 'Qiniu cloud storage',
+      external_link_kdniao_api: 'Kdniao API'
     }
   },
   button: {
--- src/locales/zh.js
+++ src/locales/zh.js
@@ -31,165 +31,61 @@ export default {
       logout: '退出'
     }
   },
-  sidebar: {
-    dashboard: {
-      title: '首页'
-    },
-    user: {
-      title: '用户管理'
-    },
-    user_user: {
-      title: '会员管理'
-    },
-    user_address: {
-      title: '收货地址'
-    },
-    user_collect: {
-      title: '会员收藏'
-    },
-    user_footprint: {
-      title: '会员足迹'
-    },
-    user_history: {
-      title: '搜索历史'
-    },
-    user_feedback: {
-      title: '意见反馈'
-    },
-    mall: {
-      title: '商场管理'
-    },
-    mall_region: {
-      title: '行政区域'
-    },
-    mall_brand: {
-      title: '品牌制造商'
-    },
-    mall_category: {
-      title: '商品类目'
-    },
-    mall_order: {
-      title: '订单管理'
-    },
-    mall_aftersale: {
-      title: '售后管理'
-    },
-    mall_issue: {
-      title: '通用问题'
-    },
-    mall_keyword: {
-      title: '关键词'
-    },
-    goods: {
-      title: '商品管理'
-    },
-    goods_list: {
-      title: '商品列表'
-    },
-    goods_create: {
-      title: '商品上架'
-    },
-    goods_edit: {
-      title: '商品编辑'
-    },
-    goods_comment: {
-      title: '商品评论'
-    },
-    promotion: {
-      title: '推广管理'
-    },
-    promotion_ad: {
-      title: '广告管理'
-    },
-    promotion_coupon: {
-      title: '优惠券管理'
-    },
-    promotion_coupon_detail: {
-      title: '优惠券详情'
-    },
-    promotion_topic: {
-      title: '专题管理'
-    },
-    promotion_topic_create: {
-      title: '专题创建'
-    },
-    promotion_topic_edit: {
-      title: '专题编辑'
-    },
-    promotion_groupon_rule: {
-      title: '团购规则'
-    },
-    promotion_groupon_activity: {
-      title: '团购活动'
-    },
-    sys: {
-      title: '系统管理'
-    },
-    sys_admin: {
-      title: '管理员'
-    },
-    sys_notice: {
-      title: '通知管理'
-    },
-    sys_log: {
-      title: '操作日志'
-    },
-    sys_role: {
-      title: '角色管理'
-    },
-    sys_os: {
-      title: '对象存储'
-    },
-    config: {
-      title: '配置管理'
-    },
-    config_mall: {
-      title: '商场配置'
-    },
-    config_express: {
-      title: '运费配置'
-    },
-    config_order: {
-      title: '订单配置'
-    },
-    config_wx: {
-      title: '小程序配置'
-    },
-    stat: {
-      title: '统计报表'
-    },
-    stat_user: {
-      title: '用户统计'
-    },
-    stat_order: {
-      title: '订单统计'
-    },
-    stat_goods: {
-      title: '商品统计'
-    },
-    external_link: {
-      title: '外链'
-    },
-    external_link_tencent_cos: {
-      title: '腾讯云存储'
-    },
-    external_link_tencent_sms: {
-      title: '腾讯云短信'
-    },
-    external_link_weixin_pay: {
-      title: '微信支付'
-    },
-    external_link_weixin_mpkf: {
-      title: '小程序客服'
-    },
-    external_link_alibaba_oss: {
-      title: '阿里云存储'
-    },
-    external_link_qiniu_kodo: {
-      title: '七牛云存储'
-    },
-    external_link_kdniao_api: {
-      title: '快递鸟'
+  app: {
+    menu: {
+      dashboard: '首页',
+      user: '用户管理',
+      user_user: '会员管理',
+      user_address: '收货地址',
+      user_collect: '会员收藏',
+      user_footprint: '会员足迹',
+      user_history: '搜索历史',
+      user_feedback: '意见反馈',
+      mall: '商场管理',
+      mall_region: '行政区域',
+      mall_brand: '品牌制造商',
+      mall_category: '商品类目',
+      mall_order: '订单管理',
+      mall_aftersale: '售后管理',
+      mall_issue: '通用问题',
+      mall_keyword: '关键词',
+      goods: '商品管理',
+      goods_list: '商品列表',
+      goods_create: '商品上架',
+      goods_edit: '商品编辑',
+      goods_comment: '商品评论',
+      promotion: '推广管理',
+      promotion_ad: '广告管理',
+      promotion_coupon: '优惠券管理',
+      promotion_coupon_detail: '优惠券详情',
+      promotion_topic: '专题管理',
+      promotion_topic_create: '专题创建',
+      promotion_topic_edit: '专题编辑',
+      promotion_groupon_rule: '团购规则',
+      promotion_groupon_activity: '团购活动',
+      sys: '系统管理',
+      sys_admin: '管理员',
+      sys_notice: '通知管理',
+      sys_log: '操作日志',
+      sys_role: '角色管理',
+      sys_os: '对象存储',
+      config: '配置管理',
+      config_mall: '商场配置',
+      config_express: '运费配置',
+      config_order: '订单配置',
+      config_wx: '小程序配置',
+      stat: '统计报表',
+      stat_user: '用户统计',
+      stat_order: '订单统计',
+      stat_goods: '商品统计',
+      external_link: '外链',
+      external_link_tencent_cos: '腾讯云存储',
+      external_link_tencent_sms: '腾讯云短信',
+      external_link_weixin_pay: '微信支付',
+      external_link_weixin_mpkf: '小程序客服',
+      external_link_alibaba_oss: '阿里云存储',
+      external_link_qiniu_kodo: '七牛云存储',
+      external_link_kdniao_api: '快递鸟'
     }
   },
   button: {
